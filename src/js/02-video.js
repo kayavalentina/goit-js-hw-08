abd7e939186da3cc60945de9f53392d18b39abc0
kayavalentina <kayavalentina1993@gmail.com>
2023-09-06 18:20:53 +0200

    Finish task2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,28 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle'; 
+
+const iframe = document.getElementById('vimeo-player');
+const player = new Player(iframe);
+
+const LOCAL_STORAGE_KEY = 'videoplayer-current-time';
+
+
+function saveCurrentTime(time) {
+  localStorage.setItem(LOCAL_STORAGE_KEY, time);
+}
+
+
+function loadCurrentTime() {
+  const savedTime = localStorage.getItem(LOCAL_STORAGE_KEY);
+  return savedTime ? parseFloat(savedTime) : 0;
+}
+
+
+player.setCurrentTime(loadCurrentTime());
+
+
+player.on('timeupdate', throttle((data) => {
+  saveCurrentTime(data.seconds);
+}, 1000));
+
 
