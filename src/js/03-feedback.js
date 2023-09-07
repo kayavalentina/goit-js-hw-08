74543fe5ca2f15e95884dd8a5b0f81cdfc5f1115
kayavalentina <kayavalentina1993@gmail.com>
2023-09-07 21:55:01 +0200

    task3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,41 @@
+import throttle from 'lodash.throttle';
 
+const feedbackForm = document.querySelector('.feedback-form');
+const emailInput = feedbackForm.querySelector('input[name="email"]');
+const messageInput = feedbackForm.querySelector('textarea[name="message"]');
+
+const saveFormDataToLocalStorage = throttle(function () {
+  const formData = {
+    email: emailInput.value,
+    message: messageInput.value,
+  };
+  localStorage.setItem('feedback-form-state', JSON.stringify(formData));
+}, 500); 
+
+document.addEventListener('DOMContentLoaded', function () {
+  const savedFormData = localStorage.getItem('feedback-form-state');
+
+  if (savedFormData) {
+    const formData = JSON.parse(savedFormData);
+    emailInput.value = formData.email;
+    messageInput.value = formData.message;
+  }
+});
+
+feedbackForm.addEventListener('submit', function (e) {
+  e.preventDefault();
+
+  const formData = {
+    email: emailInput.value,
+    message: messageInput.value,
+  };
+
+  console.log(formData);
+
+  localStorage.removeItem('feedback-form-state');
+  emailInput.value = '';
+  messageInput.value = '';
+});
+
+emailInput.addEventListener('input', saveFormDataToLocalStorage);
+messageInput.addEventListener('input', saveFormDataToLocalStorage);
