ae942b3266bc29e9ab9bb8192f0b99dafae25323
kayavalentina <kayavalentina1993@gmail.com>
2023-09-07 21:43:35 +0200

    task2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,28 +1,26 @@
 import Player from '@vimeo/player';
-import throttle from 'lodash.throttle'; 
+import throttle from 'lodash.throttle';
+
 
 const iframe = document.getElementById('vimeo-player');
 const player = new Player(iframe);
 
-const LOCAL_STORAGE_KEY = 'videoplayer-current-time';
+const saveCurrentTimeThrottled = throttle(function(currentTime) {
+  localStorage.setItem('videoplayer-current-time', currentTime.toFixed(2));
+}, 1000); 
+
+player.on('timeupdate', function(data) {
+  const currentTime = data.seconds; 
+ 
+  localStorage.setItem('videoplayer-current-time', currentTime.toFixed(2)); // Округлюємо до двох знаків після коми
 
 
-function saveCurrentTime(time) {
-  localStorage.setItem(LOCAL_STORAGE_KEY, time);
-}
+  saveCurrentTimeThrottled(currentTime);
+});
 
-
-function loadCurrentTime() {
-  const savedTime = localStorage.getItem(LOCAL_STORAGE_KEY);
-  return savedTime ? parseFloat(savedTime) : 0;
-}
-
-
-player.setCurrentTime(loadCurrentTime());
-
-
-player.on('timeupdate', throttle((data) => {
-  saveCurrentTime(data.seconds);
-}, 1000));
-
-
+document.addEventListener('DOMContentLoaded', function() {
+  const savedTime = localStorage.getItem('videoplayer-current-time');
+  if (savedTime) {
+    player.setCurrentTime(parseFloat(savedTime));
+  }
+});
